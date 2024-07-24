31a0a1b642ff911b59cb2530c15708e9e530fc8e
sriramsachitchunduri <csriramsachit@gmail.com>
2024-07-24 16:28:47 -0400

    This is first commit for config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# This file contains the API key, Secret Key and the Ticker going to be used.
+
+API_KEY = 'PKUK361LGJYPE1QGFHNN'
+SECRET_KEY = 'Wa9XCB47eBgk9RcC6f29TCtyscYqt6WpJEXHDeYW'
+TICKER_SYMBOL = 'AAPL'
