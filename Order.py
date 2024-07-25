dca650cafaaa2e7ccd00edd633857031b27c10a9
sriramsachitchunduri <csriramsachit@gmail.com>
2024-07-24 21:39:13 -0400

    added new function to cancle orders

--- Order.py
+++ Order.py
@@ -3,6 +3,7 @@ from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import LimitOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 from alpaca.trading.enums import OrderSide, QueryOrderStatus
+from alpaca.trading.requests import MarketOrderRequest
  
 API_KEY = config.API_KEY
 SECERET_KEY = config.SECRET_KEY
@@ -10,7 +11,7 @@ TICKER = config.TICKER_SYMBOL
 
 trading_client = TradingClient(API_KEY, SECERET_KEY, paper=True)
 
-def LimitaOrderSell(limit,qty):
+def LimitOrderSell(limit,qty):
 
     limit_order_data = LimitOrderRequest(
                         symbol=TICKER,
@@ -25,7 +26,7 @@ def LimitaOrderSell(limit,qty):
                     order_data=limit_order_data
                 )
 
-def LimitaOrderBuy(limit,qty):
+def LimitOrderBuy(limit,qty):
 
     limit_order_data = LimitOrderRequest(
                         symbol=TICKER,
@@ -40,6 +41,8 @@ def LimitaOrderBuy(limit,qty):
                     order_data=limit_order_data
                 )
 
+def CloseAllPositions():
+    trading_client.close_all_positions(cancel_orders=True)
 
 
 
@@ -50,4 +53,20 @@ def KillSwicth():
 def LivePositions():
 
     # to get all live positions 
-    trading_client.get_all_positions()
+    return trading_client.get_all_positions()
+    
+
+if __name__ == "__main__":
+
+    market_order_data = MarketOrderRequest(
+                    symbol="SPY",
+                    qty=0.023,
+                    side=OrderSide.BUY,
+                    time_in_force=TimeInForce.DAY
+                    )
+
+    # Market order
+    market_order = trading_client.submit_order(
+                    order_data=market_order_data
+               )
+    print(LivePositions())
