e9a4cd485a4fec5c9199ddd5d41ac3fbc7c5ddcb
sriramsachitchunduri <csriramsachit@gmail.com>
2024-07-26 03:34:21 -0400

    Normal Commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # This file contains the API key, Secret Key and the Ticker going to be used.
 
-API_KEY = ''
-SECRET_KEY = ''
-TICKER_SYMBOL = 'AAPL'
+API_KEY = 'PKFTI5D0VRYX3CRGP2DB'
+SECRET_KEY = 'uKmdO9XuzsVFudrQEzespkJ51BKeiOOrHZ8HhHdj'
+TICKER_SYMBOL = 'BCH/USD'
