c75579204fbdc6af52e8432e3c7070ed535a62b9
sriramsachitchunduri <csriramsachit@gmail.com>
2024-07-26 02:07:24 -0400

    created new function to fetch crypto data fetchCrypto()

--- StockData.py
+++ StockData.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import config
+import os 
 
 from datetime import date
 from datetime import timedelta
+
+from alpaca.data.historical import CryptoHistoricalDataClient
+from alpaca.data.requests import CryptoBarsRequest
 
 from alpaca.data import  StockHistoricalDataClient
 from alpaca.data.timeframe import TimeFrame
@@ -12,6 +16,7 @@ API_KEY = config.API_KEY
 API_SECRETKEY = config.SECRET_KEY
 TICKER = config.TICKER_SYMBOL
 
+client = CryptoHistoricalDataClient()
 stock_client = StockHistoricalDataClient(API_KEY,API_SECRETKEY)
 
 today = str(date.today())
@@ -32,7 +37,10 @@ requestParams_minute = StockBarsRequest(
   start             = yesterday,
   end               = today,
   adjustment        = 'all'
+
 )
+
+
 def fetchHS(choice):
     match choice:
         case "Hour":
@@ -55,9 +63,57 @@ def fetchHS(choice):
             print(df.tail())
             return df
         
+def fetchCrypto(choice):
+    match choice:
+        case "Hour":
+          request_params = CryptoBarsRequest(
+                              symbol=[TICKER],
+                              timeframe=TimeFrame.Hour,
+                              start='2018-01-01',
+                              end=today
+                      )
+
+          bars = client.get_crypto_bars(request_params)
+        
+          df = bars.df
+          df.reset_index(inplace=True)
+          df = df.drop(['symbol'], axis=1) 
+          csvName = "HS_"+"BTC" + "_" + today + "_Hour.csv"
+
+          df.to_csv(csvName, index=False)
+          print(df.head())
+          print(df.tail())
+          return df 
+        case "Minute":
+          request_params = CryptoBarsRequest(
+                              symbol=[TICKER],
+                              timeframe=TimeFrame.Minute,
+                              start='2021-01-01',
+                              end=today
+                      )
+
+          bars = client.get_crypto_bars(request_params)
+        
+          df = bars.df
+          df.reset_index(inplace=True)
+          df = df.drop(['symbol'], axis=1) 
+          csvName = "HS_"+"BTC" + "_" + today + "_Minute.csv"
+
+          df.to_csv(csvName, index=False)
+          print(df.head())
+          print(df.tail())
+          return df 
+        
+        
     
 if __name__ == "__main__":
-    fetchHS("Hour")  
-    
+
+  df = fetchCrypto("Hour")
+  print(df.head())
+  print(df.tail())
+
   
+  df = fetchHS("Hour")
+  print(df.head())
+  print(df.tail())
     
