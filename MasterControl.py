4a847db47fc874ba293c98253888622284ab347a
sriramsachitchunduri <csriramsachit@gmail.com>
2024-07-26 01:55:45 -0400

    updated code to handel live() properly

--- MasterControl.py
+++ MasterControl.py
@@ -4,32 +4,22 @@ import PredictionML
 import LiveData
 import Order
 import pandas as pd 
+import time 
 
 
-
-df_HS = StockData.fetchHS('Hour')
+df_HS = StockData.fetchCrypto()
 print ('Stock Data recived')
 
 df_TrainData = CleanData.fetch_trainData()
 print("Training data Fetched")
 
-df_LD = CleanData.fetch_liveData()
-print("Live Data Cleaned")
+print(df_TrainData.tail())
 
-
-X, y, df = PredictionML.load_and_preprocess_data(df_TrainData)
+X, y, df, features = PredictionML.load_and_preprocess_data(df_TrainData)
 model, scaler = PredictionML.train_model(X, y)
 print("Model Trained")
 
-print(df_LD.dtypes)
-latest_data = df_LD
-print(df_LD.tail())
-live_signal = PredictionML.live_predict(model, scaler, latest_data)
-print(live_signal)
-
-last_close = df_LD['Close'].tail()
-
-def order(live_signal):
+def order(live_signal,last_close):
     match live_signal:
         case 1:
             TP,SL,qty,Limit_price = Order.set_trade_parameters(last_close)
@@ -37,5 +27,34 @@ def order(live_signal):
             print("Order placed ")
         case default :
             print ("no Signel is generated")
-            
+
+# Start the WebSocket connection in a separate thread
+websocket_thread = LiveData.run_websocket_in_thread()
+
+# Initialize a variable to store the last processed timestamp
+last_processed_timestamp = None
+
+while True:
+    # Fetch the latest live data
+    df_LD = CleanData.fetch_liveData()
+    
+    # Check if there's new data
+    if not df_LD.empty and (last_processed_timestamp is None or df_LD.index[-1] > last_processed_timestamp):
+        print("New live data received")
+        
+        # Update the last processed timestamp
+        last_processed_timestamp = df_LD.index[-1]
+        
+        # Process the new data
+        latest_data = df_LD.tail(1)
+        lates_price = df_LD.iloc[-1].at["Close"]
+        live_signal = PredictionML.live_predict(model, scaler, latest_data, features)
+        print(f"Live signal: {live_signal}")
+
+        
+        # Place order based on the signal
+        order(live_signal,lates_price)
+    
+    # Sleep for a short interval before checking for new data again
+    time.sleep(2)  # Adjust this value as needed
 
