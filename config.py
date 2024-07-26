7dbb2b68ad52ee9f55a334535aac2a4443a88077
sriramsachitchunduri <csriramsachit@gmail.com>
2024-07-26 03:52:11 -0400

    Config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # This file contains the API key, Secret Key and the Ticker going to be used.
 
-API_KEY = 'PKFTI5D0VRYX3CRGP2DB'
-SECRET_KEY = 'uKmdO9XuzsVFudrQEzespkJ51BKeiOOrHZ8HhHdj'
+API_KEY = ''
+SECRET_KEY = ''
 TICKER_SYMBOL = 'BCH/USD'
